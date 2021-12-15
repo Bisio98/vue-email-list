bb1c1682f618d03d9d8d6365136f3ac45650bf64
Bisio98 <alberto.bisetti@hotmail.it>
2021-12-15 14:49:33 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,25 @@
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            maxNumberMail: 10,
+            eMails: [],
+        },
+        methods: {
+            getRandomNumbers: function() {
+                for(let i = 0; i < this.maxNumberMail; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail',
+                ).then((response) => { 
+                        this.eMails.push(response.data.response)
+                    
+                });
+                }
+            }
+        },
+        created: function() {
+            this.getRandomNumbers();
+        }
+    }
+);
